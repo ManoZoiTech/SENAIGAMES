41212a572ab289ff8c7f9ec61baa037e1bcc0de0
ManoZoiTech <[evanilson4002@gmail.com]>
2025-08-20 17:58:27 -0300

    ok

--- marketplace/models.py
+++ marketplace/models.py
@@ -8,11 +8,13 @@ from django.db import models
 # localhost:8000/site/admin
 # github
 
-
-
 # herança 
 class Membro(models.Model):
     email = models.CharField(max_length=50,null=False,blank=False)
     senha = models.CharField(max_length=50,null=False,blank=False)
+    nome = models.CharField(max_length=80,null=False,blank=False)
+
+    def __self__ (self):
+        return f"Nome [nome={self.nome}]"
 
  
--- marketplace/views.py
+++ marketplace/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from marketplace.models import Membro
 # Create your views here.
 def index(request):
 
@@ -14,10 +14,12 @@ def index(request):
     return render(request, 'marketplace/index.html')
         
 def autenticar_membro(request):
-    dados = {
-        1: {"nome":"Visual Novel"},
-        2: {"nome":"Plataforma"},
-        3: {"nome":"Acao"}
+    # dados = {
+    #     1: {"nome":"Visual Novel"},
+    #     2: {"nome":"Plataforma"},
+    #     3: {"nome":"Acao"}
         
-    }
+    # }
+
+    membros = Membro.objects.all()
     return render(request, 'marketplace/sou_membro.html', {'cards': dados})
